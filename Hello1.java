3e27b77207560498ff9cd7d16a057a2e8b075b2c
JANANI.V.S <jananisellamuthu97@gmail.com>
2017-07-04 17:18:11 +0530

    Create Hello1.java

---
+++ Hello1.java
@@ -0,0 +1,14 @@
+# Hello
+import java.io.*;
+import java.util.*;
+public class Hello1
+{
+public static void main(String args[])throws IOException
+{
+BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+for(int i=0;i<=5;i++)
+{
+System.out.println("Hello");
+}
+}
+}
